15c0cf67bfa1acae86277a95df6cc5b36c341767
TrinidadHype <mitra.sahatoo@gmail.com>
2022-08-10 16:06:39 -0600

    added wallet connect and fixes styling

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import * as web3 from "@solana/web3.js";
+
+require("../styles/walletmulti.css");
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => web3.clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,21 +1,23 @@
-import type { NextPage } from 'next'
-import { FaTwitter } from 'react-icons/fa'
+import type { NextPage } from "next";
+import { FaTwitter } from "react-icons/fa";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
-const TWITTER_HANDLE = 'trinidadhype';
+const TWITTER_HANDLE = "trinidadhype";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home: NextPage = () => {
   return (
     <div className="h-screen bg-[#1a202c] overflow-auto text-center">
       <div className="h-screen flex relative flex-col justify-center px-8">
-        <div>
+        <div className="">
           <p className="m-0 text-5xl font-bold text-white">🖼 GIF Portal</p>
           <p className="text-2xl text-white">
             View your GIF collection in the metaverse ✨
           </p>
+          <WalletMultiButton />
         </div>
         <div className="flex justify-center items-center absolute w-full bottom-0 left-0 pb-11">
-          <FaTwitter color='white' className='mr-2'/>
+          <FaTwitter color="white" className="mr-2" />
           <a
             className="text-white text-base font-bold"
             href={TWITTER_LINK}
@@ -26,6 +28,6 @@ const Home: NextPage = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
