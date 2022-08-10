90a31ce5975abf343e367fbbe936a1e209036ffc
TrinidadHype <mitra.sahatoo@gmail.com>
2022-08-10 12:55:26 -0600

    added buildspace styles

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,14 +1,31 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { FaTwitter } from 'react-icons/fa'
+
+const TWITTER_HANDLE = 'trinidadhype';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="h-screen bg-[#1a202c] overflow-auto text-center">
+      <div className="h-screen flex relative flex-col justify-center px-8">
+        <div>
+          <p className="m-0 text-5xl font-bold text-white">🖼 GIF Portal</p>
+          <p className="text-2xl text-white">
+            View your GIF collection in the metaverse ✨
+          </p>
+        </div>
+        <div className="flex justify-center items-center absolute w-full bottom-0 left-0 pb-11">
+          <FaTwitter color='white' className='mr-2'/>
+          <a
+            className="text-white text-base font-bold"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home
