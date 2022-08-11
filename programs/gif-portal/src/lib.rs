c4e2fab482029dc98bf2d7941a47372cce433fbf
TrinidadHype <mitra.sahatoo@gmail.com>
2022-08-10 21:02:37 -0600

    setup gif account code

--- programs/gif-portal/src/lib.rs
+++ programs/gif-portal/src/lib.rs
@@ -1,15 +1,29 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GWnfF71VZSD6teEg6qjrCycnVAaidHFg8z4VHpJpdwyr");
 
 #[program]
 pub mod gif_portal {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitGIF>) -> Result<()> {
+        let gif_account = &mut ctx.accounts.gif_account;
+        gif_account.num_gifs = 0;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitGIF<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub gif_account: Account<'info, GifAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct GifAccount {
+    pub num_gifs: u64,
+}
--- tests/gif-portal.ts
+++ tests/gif-portal.ts
@@ -2,15 +2,27 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { GifPortal } from "../target/types/gif_portal";
 
+const { SystemProgram } = anchor.web3;
+
 describe("gif-portal", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.GifPortal as Program<GifPortal>;
+  const gifAccount = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize().accounts({
+      gifAccount: gifAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+  }).signers([gifAccount]).rpc();
+
+    console.log("Your transaction signature:", tx);
+
+    let account = await program.account.gifAccount.fetch(gifAccount.publicKey);
+    console.log('👀 GIF Count', account.numGifs.toString())
   });
 });
